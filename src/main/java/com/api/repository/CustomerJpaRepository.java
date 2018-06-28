651364539454346cbe163965e040e99953c3b468
Leandro <leanlr96@gmail.com>
2018-06-28 12:17:15 +0100

    Denying access to people who don´t have "admin" role Besides, there is deprecated warning with the passwordEncoder that I should be solved

--- src/main/java/com/api/repository/CustomerJpaRepository.java
+++ src/main/java/com/api/repository/CustomerJpaRepository.java
@@ -1,11 +1,13 @@
 package com.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 import com.api.model.Customer;
 
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 @Repository
 public interface CustomerJpaRepository extends JpaRepository<Customer, Long> {
-	
+
 }
